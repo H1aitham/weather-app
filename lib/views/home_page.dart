db9ad15aa8ad147eeb06a2a85394efefaa24dfbb
haitham <saadh7144@gmail.com>
2024-07-25 19:15:12 +0300

    text field

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -7,14 +7,33 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
         title: const Text(
           "Search a City",
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.orange,
-          iconTheme: const IconThemeData(
+        iconTheme: const IconThemeData(
           color: Colors.white, // This changes the color of the back button
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: TextField(
+            decoration: InputDecoration(
+              label: const Text("Search",style: TextStyle(color: Colors.orange),),
+              contentPadding: const EdgeInsets.all(40),
+              hintText: 'Enter City Name',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.orange, width: 2.0),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              suffix: const Icon(Icons.search, color: Colors.orange),
+            ),
+          ),
         ),
       ),
     );
