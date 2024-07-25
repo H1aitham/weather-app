843ce45e735886a7fa2b81d8edb2b1a99b3d8df5
haitham <saadh7144@gmail.com>
2024-07-26 01:04:07 +0300

    http package

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,3 +1,9 @@
+import 'package:http/http.dart' as http;
+
 class WeatherServices {
-  void getWeather({required String cityName}) {}
+  void getWeather({required String cityName}) async {
+    Uri url = Uri.parse(
+        'http://api.weatherapi.com/v1/current.json?key=2cc019babc10409dbb5164407242407&q=London&aqi=no');
+    http.Response response = await http.get(url);
+  }
 }
