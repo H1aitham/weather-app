01df312a171a03dfbc5777ae97a16686fa5f7518
haitham <saadh7144@gmail.com>
2024-07-30 19:33:10 +0300

    return weather

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
@@ -6,7 +7,7 @@ import 'package:weather_app/model/weather_model.dart';
 class WeatherServices {
   String baseUrl = "http://api.weatherapi.com/v1";
   String apiKey = "2cc019babc10409dbb5164407242407";
-  void getWeather({required String cityName}) async {
+  Future<WeatherModel> getWeather({required String cityName}) async {
     Uri url =
         Uri.parse('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
     http.Response response = await http.get(url);
@@ -16,10 +17,12 @@ class WeatherServices {
     var jsonData = data['forecast']['forecastday'][0];
 
     WeatherModel weather = WeatherModel(
-        date: data['location']['localtime'],
-        temp: jsonData['avgtemp_c'],
-        maxTemp: jsonData['maxtemp_c'],
-        minTemp: jsonData['maxtemp_c'],
-        weatherSateName:  jsonData['condition']['text'],);
+      date: data['location']['localtime'],
+      temp: jsonData['avgtemp_c'],
+      maxTemp: jsonData['maxtemp_c'],
+      minTemp: jsonData['maxtemp_c'],
+      weatherSateName: jsonData['condition']['text'],
+    );
+    return weather;
   }
 }
